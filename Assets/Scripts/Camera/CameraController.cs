2c6c9b25b2928495a1ea0d0f09d713da1ab06bcc
raulsg3 <raulseguragaleote@gmail.com>
2022-02-11 21:21:05 +0100

    Camera zoom using mouse scrolling. Changed sensitivity to use similar values for zoom speed and panning speed

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public float panSpeed = 10f;
+    public float zoomSpeed = 10f;
 
     void Update()
     {
+        //Camera panning
         float verticalTranslation = Input.GetAxis("Vertical") * panSpeed * Time.deltaTime;
         float horizontalTranslation = Input.GetAxis("Horizontal") * panSpeed * Time.deltaTime;
+
+        float zoomTranslation = Input.GetAxis("Mouse ScrollWheel") * zoomSpeed * Time.deltaTime;
 
         Vector3 cameraPosition = transform.position;
 
         cameraPosition.x += horizontalTranslation;
+        cameraPosition.y -= zoomTranslation;
         cameraPosition.z += verticalTranslation;
 
         transform.position = cameraPosition;
