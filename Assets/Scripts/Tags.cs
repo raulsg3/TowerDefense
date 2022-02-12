6c0ea5a8d1e75e947c9c98eb2d8a003017f416df
raulsg3 <raulseguragaleote@gmail.com>
2022-02-12 19:08:51 +0100

    Tag PlayerBase

---
+++ Assets/Scripts/Tags.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefense
+{
+    public static class Tags
+    {
+        public static string PlayerBase = "PlayerBase";
+    }
+}
