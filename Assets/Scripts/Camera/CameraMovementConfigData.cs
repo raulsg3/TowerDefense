08cb3f7df0635a99d872a8f3f629cedf9d518285
raulsg3 <raulseguragaleote@gmail.com>
2022-02-12 18:40:33 +0100

    Default value for the camera configuration variables

--- Assets/Scripts/Camera/CameraMovementConfigData.cs
+++ Assets/Scripts/Camera/CameraMovementConfigData.cs
@@ -8,10 +8,10 @@ namespace TowerDefense
     public class CameraMovementConfigData : ScriptableObject
     {
         [SerializeField]
-        private float _panSpeed = 0f;
+        private float _panSpeed = 10f;
 
         [SerializeField]
-        private float _zoomSpeed = 0f;
+        private float _zoomSpeed = 10f;
 
         public float PanSpeed => _panSpeed;
         public float ZoomSpeed => _zoomSpeed;
