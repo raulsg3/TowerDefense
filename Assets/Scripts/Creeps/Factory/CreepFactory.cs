a4c44cf6963a457c35ec60ee874c4fa08e44c5a7
raulsg3 <raulseguragaleote@gmail.com>
2022-02-13 13:20:52 +0100

    Creep factory. Create new creeps from serialized prefabs. Store a prefab per creep type

---
+++ Assets/Scripts/Creeps/Factory/CreepFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefense
+{
+    public class CreepFactory : MonoBehaviour
+    {
+        [SerializeField]
+        private Creep[] _creeps;
+
+        private Dictionary<Creep.EType, Creep> _creepTypes;
+
+        public void Awake()
+        {
+            _creepTypes = new Dictionary<Creep.EType, Creep>();
+
+            foreach (Creep creep in _creeps)
+            {
+                _creepTypes.Add(creep.Type, creep);
+            }
+        }
+
+        public Creep CreateCreep(Creep.EType type)
+        {
+            Creep creep;
+
+            if (!_creepTypes.TryGetValue(type, out creep))
+                throw new Exception($"Creep with id {type} not found");
+
+            return Instantiate(creep);
+        }
+    }
+}
