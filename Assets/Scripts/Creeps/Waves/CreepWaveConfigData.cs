91a7c1a0a3e4a9df68f9d649478ed8f90dcaa511
raulsg3 <raulseguragaleote@gmail.com>
2022-02-13 17:30:52 +0100

    New configuration data for a concrete wave of creeps

---
+++ Assets/Scripts/Creeps/Waves/CreepWaveConfigData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefense
+{
+    [CreateAssetMenu(menuName = "TowerDefense/Creeps/Factory/Creep Wave Configuration Data", fileName = "CreepWaveConfigData")]
+    public class CreepWaveConfigData : ScriptableObject
+    {
+        [SerializeField]
+        private float _timeBetweenCreeps = 1f;
+
+        public float TimeBetweenCreeps => _timeBetweenCreeps;
+
+        [SerializeField]
+        private Creep.EType[] _waveCreeps;
+
+        private Dictionary<Creep.EType, int> _waveCreepsByType;
+
+        void Awake()
+        {
+            _waveCreepsByType = new Dictionary<Creep.EType, int>();
+
+            foreach (Creep.EType creep in _waveCreeps)
+            {
+                _waveCreepsByType.TryGetValue(creep, out var currentCreeps);
+                _waveCreepsByType[creep] = currentCreeps + 1;
+            }
+        }
+
+        public CreepWave GetCreepWave()
+        {
+            return new CreepWave(_waveCreepsByType);
+        }
+    }
+}
--- Assets/Scripts/Creeps/Waves/CreepWaveSpawner.cs
+++ Assets/Scripts/Creeps/Waves/CreepWaveSpawner.cs
@@ -1,15 +1,39 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace TowerDefense
 {
     public class CreepWaveSpawner : ICreepWaveSpawner
     {
-        private CreepFactory _creepFactory;
+        [SerializeField]
+        private CreepWaveConfigData _creepWaveConfigData;
 
-        public CreepWaveSpawner(CreepFactory creepFactory)
+        private CreepWaveConfigData _creepWaveConfigDataInstance;
+
+        [SerializeField]
+        private CreepFactoryConfigData _creepFactoryConfigData;
+
+        private ICreepFactory _creepFactory;
+
+        public CreepWaveSpawner(ICreepFactory creepFactory)
         {
             _creepFactory = creepFactory;
+        }
+
+        void Start()
+        {
+            _creepWaveConfigDataInstance = Instantiate(_creepWaveConfigData);
+            _creepFactory = new CreepFactory(Instantiate(_creepFactoryConfigData));
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                CreepWave creepWave = _creepWaveConfigDataInstance.GetCreepWave();
+                StartWaveSpawn(creepWave);
+            }
         }
 
         public override void StartWaveSpawn(CreepWave creepWave)
@@ -19,7 +43,7 @@ namespace TowerDefense
 
         private IEnumerator SpawnWave(CreepWave creepWave)
         {
-            WaitForSeconds waitBetweenCreeps = new WaitForSeconds(1);
+            WaitForSeconds waitBetweenCreeps = new WaitForSeconds(_creepWaveConfigDataInstance.TimeBetweenCreeps);
 
             foreach (var numCreepsByType in creepWave.NumCreepsByType)
             {
