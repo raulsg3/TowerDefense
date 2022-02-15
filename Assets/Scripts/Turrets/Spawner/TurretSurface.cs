c358da863916b3ee0a2eef1fcf4bf295362a6a78
raulsg3 <raulseguragaleote@gmail.com>
2022-02-15 21:14:16 +0100

    Turret placeholder hovering around on mouse position while placement mode is active

--- Assets/Scripts/Turrets/Spawner/TurretSurface.cs
+++ Assets/Scripts/Turrets/Spawner/TurretSurface.cs
@@ -4,9 +4,18 @@ namespace TowerDefense
 {
     public class TurretSurface : MonoBehaviour
     {
+        [SerializeField]
+        private GameObject _placeholderTurretPrefab;
+
+        private GameObject _placeholderTurret;
+        private Renderer _placeholderTurretRenderer;
+
         private void Start()
         {
             EventManagerSingleton.Instance.OnSetTurretPlacing += EnableTurretSurface;
+
+            _placeholderTurret = Instantiate(_placeholderTurretPrefab);
+            _placeholderTurretRenderer = _placeholderTurret.GetComponent<Renderer>();
         }
 
         private void OnDestroy()
@@ -14,29 +23,53 @@ namespace TowerDefense
             EventManagerSingleton.Instance.OnSetTurretPlacing -= EnableTurretSurface;
         }
 
+        private void OnMouseEnter()
+        {
+            if (enabled)
+                EnablePlaceholderTurretRenderer(true);
+        }
+
         private void OnMouseOver()
         {
             if (enabled)
             {
-                if (Input.GetMouseButtonDown(0))
-                {
-                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                    RaycastHit raycastHit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit raycastHit;
 
-                    if (Physics.Raycast(ray, out raycastHit, 100f))
+                if (Physics.Raycast(ray, out raycastHit, 100f))
+                {
+                    if (raycastHit.transform.gameObject == this.gameObject)
                     {
-                        if (raycastHit.collider.CompareTag(Tags.Ground))
-                        {
-                            EventManagerSingleton.Instance.TurretPositionChosen(raycastHit.point);
-                        }
+                        Vector3 placeholderTurretPosition = raycastHit.point;
+                        placeholderTurretPosition.y = _placeholderTurret.transform.position.y;
+
+                        _placeholderTurret.transform.position = placeholderTurretPosition;
+
                     }
                 }
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    EventManagerSingleton.Instance.TurretPositionChosen(_placeholderTurret.transform.position);
+                }
             }
+        }
+
+        private void OnMouseExit()
+        {
+            if (enabled)
+                EnablePlaceholderTurretRenderer(false);
         }
 
         private void EnableTurretSurface(bool enable)
         {
             enabled = enable;
+            EnablePlaceholderTurretRenderer(enable);
+        }
+
+        private void EnablePlaceholderTurretRenderer(bool enable)
+        {
+            _placeholderTurretRenderer.enabled = enable;
         }
     }
 }
