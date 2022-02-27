1e0904c150894c50de8cb297d2cee5284c0ce5d2
raulsg3 <raulseguragaleote@gmail.com>
2022-02-27 17:47:48 +0100

    Service Locator implementation as a Singleton for having global access to EventManager

---
+++ Assets/Scripts/Game/ServiceLocatorSingleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TowerDefense
+{
+    public class ServiceLocatorSingleton
+    {
+        private static ServiceLocatorSingleton _instance;
+
+        public static ServiceLocatorSingleton Instance => _instance ?? (_instance = new ServiceLocatorSingleton());
+
+        private Dictionary<Type, object> _services;
+
+        private ServiceLocatorSingleton()
+        {
+            _services = new Dictionary<Type, object>();
+        }
+
+        public void RegisterService<T>(T service)
+        {
+            Type serviceType = typeof(T);
+            _services.Add(serviceType, service);
+        }
+
+        public T GetService<T>()
+        {
+            Type serviceType = typeof(T);
+
+            if (!_services.TryGetValue(serviceType, out var service))
+                throw new Exception($"Service with type {serviceType} not registered");
+
+            return (T)service;
+        }
+    }
+}
